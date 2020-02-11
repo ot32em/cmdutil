82188891979b8f1e6cc996e7032b101ab586454b
OT Chen <ot32em@gmail.com>
2020-02-11 15:47:07 +0800

    refactoring variables

--- git_svn_clone_r.sh
+++ git_svn_clone_r.sh
@@ -10,11 +10,15 @@ fi
 
 # clone main
 git svn clone $1 $2
+if [ $? -ne 0 ]; then 
+	echo "Failed: git svn clone $1 $2, ret: $?"
+	exit;
+fi
 
 
 # clone externals
 pushd $main_path > /dev/null
-script_abs="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/$(basename $0)"
+git_svn_clone_r_path ="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/$(basename $0)"
 
 # git svn show-externals output layout example:
 # >>> /YOUR/SUB/FOLDER/http://YOUR.SVN.REPO/repos/to/module/ SUBFOLDER/IN/EXTERNAL <<<
@@ -25,12 +29,12 @@ external_path_regex=[a-zA-Z0-9/]+
 git svn show-externals | 
 while read line; do
 	if [[ $line =~ ^($dir_regex)($repo_regex)[[:space:]]($external_path_regex) ]]; then
-		dir=${BASH_REMATCH[1]}
-		repo=${BASH_REMATCH[2]}
-		path=${BASH_REMATCH[3]}
+		sub_dir=".${BASH_REMATCH[1]}"
+		repo_url=${BASH_REMATCH[2]}
+		clone_dir=${BASH_REMATCH[3]}
 		
-		pushd .$dir > /dev/null
-		eval "$script_abs $repo ${path}"
+		pushd $sub_dir > /dev/null
+		eval "$git_svn_clone_r_path $repo_url $clone_dir"
 		popd > /dev/null
 	fi
 done
