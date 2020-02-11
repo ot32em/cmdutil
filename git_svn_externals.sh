a2f21928706588c78770cbb3a8043ae90cc34d66
OT Chen <ot32em@gmail.com>
2020-02-11 15:25:01 +0800

    add comment

--- git_svn_externals.sh
+++ git_svn_externals.sh
@@ -1,7 +1,9 @@
-if [ -z $1 ]; then exit; fi
-if [ -z $2 ]; then exit; fi
+if [ -z $1 ] || [ -z $2 ]; then 
+	echo "Usage: ./XXX.sh REPO_URL CLONE_DIR"
+	exit;
+fi
 
-full="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/$(basename $0)"
+cur_script="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/$(basename $0)"
 
 # clone main
 main_repo=$1
@@ -11,6 +13,8 @@ git svn clone $main_repo $main_path
 pushd $main_path > /dev/null
 
 # clone externals
+# git svn show-externals output layout example:
+# >>> /YOUR/SUB/FOLDER/http://YOUR.SVN.REPO/repos/to/module/ SUBFOLDER/IN/EXTERNAL <<<
 dir_regex=[a-zA-Z0-9/]+
 repo_regex=http://[-a-zA-Z0-9@:%._+~#=/]+
 external_path_regex=[a-zA-Z0-9/]+
@@ -23,9 +27,9 @@ while read line; do
 		path=${BASH_REMATCH[3]}
 		
 		pushd .$dir > /dev/null
-		eval "$full $repo ${path}"
+		eval "$cur_script $repo ${path}"
 		popd > /dev/null
 	fi
 done
 
-popd > /dev/null
+popd > /dev/null # resume folder position changed in line 13
