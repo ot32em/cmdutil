3195ebed3f199c27ab80da600c5737e22cd4575e
OT Chen <ot32em@gmail.com>
2020-02-11 15:17:51 +0800

    make fully recursively

--- git_svn_externals.sh
+++ git_svn_externals.sh
@@ -1,1 +1,31 @@
-git svn show-externals | awk 'BEGIN{FS=" "}/^[^#]/{split($1, a, "http");printf("git svn clone http%s ./%s%s\n", a[2], a[1], $2)}' | bash
+if [ -z $1 ]; then exit; fi
+if [ -z $2 ]; then exit; fi
+
+full="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/$(basename $0)"
+
+# clone main
+main_repo=$1
+main_path=$2
+
+git svn clone $main_repo $main_path
+pushd $main_path > /dev/null
+
+# clone externals
+dir_regex=[a-zA-Z0-9/]+
+repo_regex=http://[-a-zA-Z0-9@:%._+~#=/]+
+external_path_regex=[a-zA-Z0-9/]+
+
+git svn show-externals | 
+while read line; do
+	if [[ $line =~ ^($dir_regex)($repo_regex)[[:space:]]($external_path_regex) ]]; then
+		dir=${BASH_REMATCH[1]}
+		repo=${BASH_REMATCH[2]}
+		path=${BASH_REMATCH[3]}
+		
+		pushd .$dir > /dev/null
+		eval "$full $repo ${path}"
+		popd > /dev/null
+	fi
+done
+
+popd > /dev/null
