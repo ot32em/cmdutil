3e70378cf9cfa12bd9665a8e45abf9b889d5baec
OT Chen <ot32em@gmail.com>
2020-02-12 10:06:34 +0800

    add git_pwd to locate .git dir

--- split_git_svn_externals.sh
+++ split_git_svn_externals.sh
@@ -6,10 +6,11 @@
 dir_regex=[a-zA-Z0-9/]+
 repo_regex=http[s]?://[-a-zA-Z0-9@:%._+~#=/]+
 external_path_regex=[a-zA-Z0-9/]+
+git_pwd=$(git rev-parse --show-toplevel)
 
 while read -r line; do
 	if [[ $line =~ ^($dir_regex)($repo_regex)[[:space:]]($external_path_regex) ]]; then
-		sub_dir=".${BASH_REMATCH[1]}"
+		sub_dir="${git_pwd}${BASH_REMATCH[1]}"
 		repo_url=${BASH_REMATCH[2]}
 		clone_dir=${BASH_REMATCH[3]}
 		
