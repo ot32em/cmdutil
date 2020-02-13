7def0f36c9597ddca398e957722fcc2e585131e0
OT Chen <ot32em@gmail.com>
2020-02-13 10:16:25 +0800

    rename git_svn_XXX.sh to gs-XXX without sh

--- ls_git_svn_externals.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -z $1 ]; then 
-	git svn show-externals | sh split_git_svn_externals.sh
-elif [[ $1 =~ ^[0-9] ]]; then
-	git svn show-externals | sh split_git_svn_externals.sh | cut -d ' ' -f $1
-fi
-
